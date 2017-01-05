1ccc44e4e3bb74419e52e8756ed0f5eb57468c14
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2017-01-05 16:15:08 -0600

    stretchy buffer working

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -2,19 +2,47 @@
 #include <windows.h>
 #include <stdint.h>
 
+// structs
+struct win32_Buffer {
+	BITMAPINFO BitmapInfo;
+	void* BitmapMemory;
+	int Width;
+	int Height;
+	int Pitch;
+	int BytesPerPixel;
+};
+
+struct win32_WinDim {
+	int Width;
+	int Height;
+};
+
 // globals
 static bool Running;
-static BITMAPINFO BitmapInfo;
-static void* BitmapMemory;
+static win32_Buffer GlobalBackBuffer;
+
+// helper functions
+static win32_WinDim GetWinDim(HWND window) {
+	win32_WinDim res;
+   	RECT ClientRect;
+    GetClientRect(window, &ClientRect);
+	res.Width = ClientRect.right - ClientRect.left;
+	res.Height = ClientRect.bottom - ClientRect.top;
+	return res;
+}
 
 static void
-RenderWeirdGradient(int Width, int Height, int BytesPerPixel, int xOffset, int yOffset) 
+RenderWeirdGradient(win32_Buffer buffer, int xOffset, int yOffset)
 {
-	uint8_t* row = (uint8_t*) BitmapMemory;
-	for (int y = 0; y < Height; ++y) {
-		
+	int Width = buffer.Width;
+	int Height = buffer.Height;
+	int BytesPerPixel = buffer.BytesPerPixel;
+
+	uint8_t* row = (uint8_t*) buffer.BitmapMemory;
+	for (int y = 0; y < buffer.Height; ++y) {
+
 		uint32_t* pixel = (uint32_t*) row;
-		for (int x = 0; x < Width; ++x) {
+		for (int x = 0; x < buffer.Width; ++x) {
 			//   Memory:   BB GG RR XX
 			//   Register: XX RR GG BB
 			uint8_t blue = (x + xOffset);
@@ -24,43 +52,50 @@ RenderWeirdGradient(int Width, int Height, int BytesPerPixel, int xOffset, int y
 			pixel += 1;
 		}
 
-		row += Width * BytesPerPixel;
+		row += buffer.Pitch;
 	}
 }
 
 // Create the buffer that we will have Windows display for us
 // Device Independent Bitmap
 static void
-ResizeDibSection(int Width, int Height)
+ResizeDibSection(win32_Buffer* buffer, int Width, int Height)
 {
 	// Free our old DIB section if we ask for a new one
-	if (BitmapMemory) { VirtualFree(BitmapMemory, 0, MEM_RELEASE); }
-	
-	// setup the bit map info header
-	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-	BitmapInfo.bmiHeader.biWidth = Width;
-	BitmapInfo.bmiHeader.biHeight = -Height;
-	BitmapInfo.bmiHeader.biPlanes = 1;
-	BitmapInfo.bmiHeader.biBitCount = 32;
-	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	if (buffer->BitmapMemory) { VirtualFree(buffer->BitmapMemory, 0, MEM_RELEASE); }
+
+	// Setup the buffer
+	buffer->Width = Width; buffer->Height = Height;
+	buffer->BytesPerPixel = 4;
+	buffer->Pitch = buffer->Width * buffer->BytesPerPixel;
+
+	// Setup the bit map info header
+	buffer->BitmapInfo.bmiHeader.biSize = sizeof(buffer->BitmapInfo.bmiHeader);
+	buffer->BitmapInfo.bmiHeader.biWidth = buffer->Width;
+	buffer->BitmapInfo.bmiHeader.biHeight = -1 * buffer->Height;
+	buffer->BitmapInfo.bmiHeader.biPlanes = 1;
+	buffer->BitmapInfo.bmiHeader.biBitCount = 32;
+	buffer->BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
 	// Allocate memory for our DIB section
-	int BytesPerPixel = 4;
-	int BitmapMemorySize = (Width * Height) * (BytesPerPixel);
-	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+	int BitmapMemorySize = (buffer->Width * buffer->Height) * (buffer->BytesPerPixel);
+	buffer->BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
 // Have Windows output our buffer and scale it as appropriate
 static void
-DisplayDib(HDC DeviceContext, RECT* WinRect, int X, int Y, int W, int H)
+DisplayDib(HDC DeviceContext, 
+		   int WinWidth, int WinHeight,
+		   win32_Buffer buffer,
+		   int X, int Y, int W, int H)
 {
 	StretchDIBits(DeviceContext,
 		   		  /*X, Y, W, H,
 				  X, Y, W, H,*/
-				  0, 0, WinRect->right - WinRect->left, WinRect->bottom - WinRect->top,
-				  0, 0, WinRect->right - WinRect->left, WinRect->bottom - WinRect->top,
-				  BitmapMemory,
-				  &BitmapInfo,
+				  0, 0, WinWidth, WinHeight,
+				  0, 0, buffer.Width, buffer.Height,
+				  buffer.BitmapMemory,
+				  &buffer.BitmapInfo,
 				  DIB_RGB_COLORS,
 				  SRCCOPY);
 }
@@ -73,12 +108,7 @@ WindowProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 
 	// Parse Message
 	switch (Msg) {
-		case WM_SIZE:        { RECT ClientRect;
-							   GetClientRect(hwnd, &ClientRect);
-							   ResizeDibSection(ClientRect.right - ClientRect.left, // resize our DIB section
-									  			ClientRect.bottom - ClientRect.top);
-							   break;
-							 }
+		case WM_SIZE:          break;
 		case WM_CLOSE:         Running = false; break;
 		case WM_DESTROY:       Running = false; break;
 		case WM_ACTIVATEAPP:   OutputDebugStringA("WM_ACTIVATEAPP\n"); break;
@@ -89,9 +119,8 @@ WindowProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 							   int Y = Paint.rcPaint.top;
 							   int W = Paint.rcPaint.right - Paint.rcPaint.left;
 							   int H = Paint.rcPaint.bottom - Paint.rcPaint.top;
-							   RECT ClientRect;
-							   GetClientRect(hwnd, &ClientRect);
-							   DisplayDib(DeviceContext, &ClientRect, X, Y, W, H); // have Windows output our buffer
+							   win32_WinDim Dimension = GetWinDim(hwnd);
+							   DisplayDib(DeviceContext, Dimension.Width, Dimension.Height, GlobalBackBuffer, X, Y, W, H); // display our buffer
 							   EndPaint(hwnd, &Paint);
 							   break;
 							 }
@@ -109,11 +138,14 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
 {
 	// Set up WindowClass
 	WNDCLASS WindowClass = {};
-	WindowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	WindowClass.style = CS_VREDRAW | CS_HREDRAW;
 	WindowClass.lpfnWndProc = WindowProc;
 	WindowClass.hInstance = hInstance;
 // 	WindowClass.hIcon = ;
 	WindowClass.lpszClassName = "Handmade Hero Window Class";
+
+	// Set the size of our buffer
+   	ResizeDibSection(&GlobalBackBuffer, 1280, 720);
 
 	// Register the window and create the window
 	if (RegisterClass(&WindowClass)) {
@@ -124,18 +156,22 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
 			// Message queue --> pull out one at a time --> translate & dispatch --> parse in Window Callback procedure
 			Running = true; MSG Message; int xOffset = 0; int yOffset = 0;
 			while (Running) {
-				
+
 				while (PeekMessage(&Message, 0, 0, 0, PM_REMOVE)) {
 					if (Message.message == WM_QUIT) { Running = false; }
 					TranslateMessage(&Message);
 					DispatchMessageA(&Message);
 				}
-				int tempW = 4096; int tempH = 4096; HDC DeviceContext = GetDC(WindowHandle); RECT ClientRect; GetClientRect(WindowHandle, &ClientRect);
-				int w = ClientRect.right - ClientRect.left; int h = ClientRect.bottom - ClientRect.top;
-				RenderWeirdGradient(w, h, 4, xOffset, yOffset);
-				DisplayDib(DeviceContext, &ClientRect, 0, 0, w, h); // have Windows output our buffer
+				
+				// TODO: some temp stuff for displaying our gradient
+			    HDC DeviceContext = GetDC(WindowHandle); 
+				win32_WinDim Dimension = GetWinDim(WindowHandle);
+				RenderWeirdGradient(GlobalBackBuffer, xOffset, yOffset);
+				DisplayDib(DeviceContext, Dimension.Width, Dimension.Height, // have Windows output our buffer
+						   GlobalBackBuffer, 0, 0, 
+						   Dimension.Width, Dimension.Height); 
 				ReleaseDC(WindowHandle, DeviceContext);
-				++xOffset;
+				++xOffset; ++yOffset;
 			}
 		}
 	}
